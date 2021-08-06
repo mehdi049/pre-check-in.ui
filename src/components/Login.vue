82f5c266fce12b5000b4b130062aff40827e1e25
Mehdi Marouani <Mehdi.Marouani@vistaprint.com>
2021-08-06 15:52:50 +0100

    - setup font awesome - add images

---
+++ src/components/Login.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="content-center bg-gray">
+    <div class="container">
+      <div class="columns is-mobile is-centered">
+        <div
+          class="
+              column
+              is-10-mobile is-6-tablet is-6-desktop is-5-widescreen is-4-fullhd
+              box
+            "
+        >
+          <div class="field">
+            <label class="label">Booking reference</label>
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+                placeholder="Booking reference"
+              />
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Email</label>
+            <div class="control">
+              <input class="input" type="email" placeholder="Email" />
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Booking date</label>
+            <div class="columns is-mobile">
+              <div class="column is-6">
+                <div class="control">
+                  <input type="date" placeholder="Arrival date" />
+                </div>
+              </div>
+              <div class="column is-6">
+                <div class="control">
+                  <input type="date" placeholder="departure date" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="field">
+            <div class="columns is-mobile">
+              <div class="column is-6">
+                <p class="control has-icons-left">
+                  <span class="select">
+                    <select>
+                      <option selected>English</option>
+                      <option>German</option>
+                    </select>
+                  </span>
+
+                  <span class="icon is-small is-left">
+                    <font-awesome-icon :icon="['fas', 'globe']" />
+                  </span>
+                </p>
+              </div>
+              <div class="column is-6">
+                <div class="control has-text-right">
+                  <a href="my-booking.html" class="button is-info">Login</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
 
-Vue.config.productionTip = false
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faGlobe);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.config.productionTip = false;
+Vue.use(Buefy);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
